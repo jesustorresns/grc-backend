8ad1b699a43ea4602f36d131dd391fe67c4ac97b
jesustorresns <jesus.torres@newsol.com.co>
2024-12-05 12:19:24 -0500

    Configurar Swagger
    Signed-off-by: jesustorresns <jesus.torres@newsol.com.co>

---
+++ src/main/java/co/com/newsol/grcbackend/configuration/ApiSwaggerConfig.java
@@ -0,0 +1,19 @@
+package co.com.newsol.grcbackend.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiSwaggerConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("New-GRC Backend API")
+				.version("0.0.1")
+				.description("Documentación de la API de New-GRC"));
+	}
+}
