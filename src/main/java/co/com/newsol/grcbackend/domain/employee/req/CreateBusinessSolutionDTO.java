91ccef20f3e4d62613d14fc3f275fe66ee399dff
jesustorresns <jesus.torres@newsol.com.co>
2024-12-06 15:00:43 -0500

    Corregir firma
    Signed-off-by: jesustorresns <jesus.torres@newsol.com.co>

--- src/main/java/co/com/newsol/grcbackend/domain/employee/req/CreateBusinessSolutionDTO.java
+++ src/main/java/co/com/newsol/grcbackend/domain/employee/req/CreateBusinessSolutionDTO.java
@@ -1,9 +1,6 @@
 package co.com.newsol.grcbackend.domain.employee.req;
 
-import java.util.List;
-
 public record CreateBusinessSolutionDTO(
 	String solutionType,
-	List<String> solution,
-	List<String> collaborativeSolutionType) {
+	String solution) {
 }
--- src/main/java/co/com/newsol/grcbackend/domain/employee/req/CreateEmployeeDTO.java
+++ src/main/java/co/com/newsol/grcbackend/domain/employee/req/CreateEmployeeDTO.java
@@ -3,6 +3,8 @@ package co.com.newsol.grcbackend.domain.employee.req;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
+
 public record CreateEmployeeDTO(
 	String firstName,
 	String lastName,
@@ -18,13 +20,14 @@ public record CreateEmployeeDTO(
 	String company,
 	String companyName,
 	String contractType,
+	String contractNumber,
 	String directManager,
 	String workModality,
 	String costCenter,
-	Boolean managesShifts,
-	Boolean isTemporary,
+	String officeCity,
 	String jobFunction,
 	String additionalFunctions,
 	@NotNull CreateServiceRequestInformationDTO serviceRequest,
-	@NotNull CreateBusinessSolutionDTO businessSolution) {
+	@NotNull List<CreateBusinessSolutionDTO> businessSolution,
+	List<String> collaborativeSolutionType) {
 }
